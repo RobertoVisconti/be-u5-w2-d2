b270a3a6d4e1e0973d7417b63b718590ecdf70a4
Roberto Visconti <robertovisconti93@gmail.com>
2026-07-07 16:00:45 +0200

    Initial commit

---
+++ src/test/java/robertovisconti/be_u5_w2_d2/BeU5W2D2ApplicationTests.java
@@ -0,0 +1,13 @@
+package robertovisconti.be_u5_w2_d2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU5W2D2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
